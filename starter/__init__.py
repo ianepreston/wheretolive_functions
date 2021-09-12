7e4921c32545e02f99dbe37b58b7290621088b4b
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2021-09-12 01:57:02 +0000

    change to timer trigger

--- starter/__init__.py
+++ starter/__init__.py
@@ -4,7 +4,7 @@ import datetime as dt
 import azure.functions as func
 
 
-def main(req: func.HttpRequest, msg: func.Out[func.QueueMessage]) -> str:
+def main(timer: func.TimerRequest, msg: func.Out[func.QueueMessage]) -> str:
     logging.info("Triggering initial queue")
     q_text = f"rfaster_{dt.date.today()}_page_1"
     msg.set(q_text)
